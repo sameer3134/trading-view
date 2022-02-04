77114081b2732f9ef48f2021103043282379d911
Sameer <mohdsameer3134@gmail.com>
2022-02-05 03:19:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import BarChart from './charts/BarChart';
+
+function App() {
+  return (
+    <>
+    Trading view
+     <BarChart />
+    </>
+  );
+}
+
+export default App;
